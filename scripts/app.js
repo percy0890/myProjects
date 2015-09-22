b562bed0ab8edcd5ccec454e9f9639d263980f08
Ankit Bhatia <abhatia2312@gmail.com>
2015-09-22 13:37:10 +0530

    new1

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var app = angular.module('myApp',['ngRoute']);
+
+app.config(function ($routeProvider){
+  $routeProvider
+    .when('/',{templateUrl: 'views/main.html'})
+    .otherwise({redirectTo: '/'});
+});
